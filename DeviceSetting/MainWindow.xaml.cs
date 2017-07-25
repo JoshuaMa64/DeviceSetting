dd454d4a91dbd069580ef19df82cf696a9c7fde6
马骏 <joshuam@foxmail.com>
2017-07-25 08:30:54 +0800

    添加项目文件

---
+++ DeviceSetting/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace DeviceSetting
+{
+    /// <summary>
+    ///     MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private XDocument xdc;
+        private readonly List<XmlConfig> deviceList = new List<XmlConfig>();
+        private readonly List<string> nameList = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 读取 DeviceConfig.xml ，若根目录下不存在就新建一个
+            xdc = !File.Exists("DeviceConfig.xml") ? XmlConfig.Create() : XDocument.Load("DeviceConfig.xml");
+
+            // 使用 Linq 读取 DeviceConfig.xml 获取通讯方式种类并添加至 ListBox
+             var query = from item
+                in xdc.Descendants("Device")
+                select new XmlConfig
+                {
+                    Id = Convert.ToInt32(item.Attribute("id")?.Value),
+                    Name = item.Attribute("Name")?.Value,
+                    LibarayName = item.Element("LibarayName")?.Value,
+                    ClassName = item.Element("Class")?.Value,
+                    EntryPoint = item.Element("EntryPoint")?.Value
+                };
+
+            var configs = query.ToList();
+            DgParam.ItemsSource = query;
+
+            foreach (var i in configs)
+            {
+                deviceList.Add(i);
+                nameList.Add(i.Name);
+            }
+
+            LbDevice.ItemsSource = nameList;
+        }
+
+        // 退出按钮
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // 动态展示
+        private void LbDevice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TxtConfig.Text = "";
+            TxtConfig.Text += $"通讯方式： {deviceList[LbDevice.SelectedIndex].Name}\n";
+            TxtConfig.Text += $"库名： {deviceList[LbDevice.SelectedIndex].LibarayName}\n";
+            TxtConfig.Text += $"类名： {deviceList[LbDevice.SelectedIndex].ClassName}\n";
+            TxtConfig.Text += $"入口函数： {deviceList[LbDevice.SelectedIndex].EntryPoint}\n";
+        }
+
+        // TODO:保存当前配置到 DeviceConfig.xml
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ DeviceSetting/XmlConfig.cs
@@ -0,0 +1,108 @@
+﻿using System.Xml.Linq;
+
+namespace DeviceSetting
+{
+    /// <summary>
+    ///     用于操作 DeviceConfig.xml 和储存设备信息的类
+    /// </summary>
+    public class XmlConfig
+    {
+        public int Id;
+        public string Name;
+        public string LibarayName;
+        public string ClassName;
+        public string EntryPoint;
+
+        //public XmlConfig(int id, string name, string libarayName, string className, string entryPoint)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    LibarayName = libarayName;
+        //    ClassName = className;
+        //    EntryPoint = entryPoint;
+        //}
+
+        // 创建默认的 DeviceConfig.xml 并保存于根目录
+        public static XDocument Create()
+        {
+            #region CreateXml
+
+            var xdc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "no"),
+                new XElement("ROOT",
+                    new XElement("DevicePath", "Device"),
+                    new XElement("CurrrentDevice",
+                        new XAttribute("id", "2")),
+                    new XElement("Devices",
+                        new XElement("Device",
+                            new XAttribute("id", "1"),
+                            new XAttribute("Name", "ZLGCan"),
+                            new XElement("LibarayName", "ZLGCan.dll"),
+                            new XElement("Class", "ZLGCan.ZlgCanDevice"),
+                            new XElement("EntryPoint", "GetInstance"),
+                            new XElement("Param",
+                                new XElement("DeviceType", "3"),
+                                new XElement("DeviceIndex", "0"),
+                                new XElement("CanIndex", "0"),
+                                new XElement("Timer", "1c")
+                            ),
+                            new XElement("ParamConfigOption",
+                                new XElement("DeviceType",
+                                    new XElement("Type", "0"),
+                                    new XElement("Type", "1"),
+                                    new XElement("Type", "2"),
+                                    new XElement("Type", "3"),
+                                    new XElement("Type", "4")
+                                ),
+                                new XElement("DeviceIndex",
+                                    new XElement("Index", "0"),
+                                    new XElement("Index", "1")
+                                ),
+                                new XElement("CanIndex",
+                                    new XElement("Index", "0"),
+                                    new XElement("Index", "1")
+                                ),
+                                new XElement("Timer",
+                                    new XElement("Value", "1c"),
+                                    new XElement("Value", "011c")
+                                )
+                            )
+                        ),
+                        new XElement("Device",
+                            new XAttribute("id", "2"),
+                            new XAttribute("Name", "NetWork"),
+                            new XElement("LibarayName", "NetWork.dll"),
+                            new XElement("Class", "NetWork.UdpDevice"),
+                            new XElement("EntryPoint", "GetInstance"),
+                            new XElement("Param",
+                                new XElement("IPAddress", "127.0.0.1"),
+                                new XElement("Port", "8900")
+                            )
+                        ),
+                        new XElement("Device",
+                            new XAttribute("id", "3"),
+                            new XAttribute("Name", "SerialPort"),
+                            new XElement("LibarayName", "SerialPort.dll"),
+                            new XElement("Class", "SerialPort"),
+                            new XElement("EntryPoint", "GetInstance"),
+                            new XElement("Param",
+                                new XElement("COM", "1"),
+                                new XElement("BaudRate", "9600")
+                            )
+                        )
+                    )
+                )
+            );
+
+            #endregion
+
+            xdc.Save("DeviceConfig.xml");
+            return xdc;
+        }
+
+        // TODO:保存修改过后的配置文件
+        public static void Save()
+        {
+        }
+    }
+}
